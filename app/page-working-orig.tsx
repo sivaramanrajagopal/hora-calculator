a77be0ff73fe0f8dc6f8fd39172e0e5ec1bac112
SivaramanRajagopal <vrsiva78@gmail.com>
2025-02-21 07:14:57 +0530

    Initial commit

---
+++ app/page-working-orig.tsx
@@ -0,0 +1,9 @@
+import HoraCalculator from '../components/hora-calculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <HoraCalculator />
+    </main>
+  )
+}
